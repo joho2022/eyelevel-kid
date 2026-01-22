a5c751e6e54804097d7551f9fe84ab7e55ef48ab
joho2022 <josama1997@naver.com>
2026-01-22 22:54:56 +0900

    chore: add image and icon assets

---
+++ lib/shared/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const backgroundGradientStart = Color(0xFFFBF3F9); // 251,243,249
+  static const backgroundGradientEnd = Color(0xFFF2F6FF); // 242,246,255
+
+  static const borderGray = Color(0xFFE6E7EB); // 230,231,235
+
+  static const white = Color(0xFFFFFFFF);
+}
---
+++ lib/shared/theme/app_images.dart
@@ -0,0 +1,7 @@
+class AppImages {
+
+  static const appLogo = 'assets/images/app_logo.png';
+
+  static const appleIcon = 'assets/icons/auth/ic_apple.svg';
+  static const googleIcon = 'assets/icons/auth/ic_google.svg';
+}
