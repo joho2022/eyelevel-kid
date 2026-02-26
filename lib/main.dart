eacd7c904e958aa2f01a10fef35b1fa9412edd65
joho2022 <josama1997@naver.com>
2026-02-26 20:41:15 +0900

    setting: Add flutter_dotenv dependency and setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:eyelevel_kid/ui/core/routes/app_router.dart';
 import 'package:eyelevel_kid/ui/core/widgets/app_background.dart';
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  dotenv.load(fileName: ".env");
   MobileAds.instance.initialize();
 
   runApp(const MyApp());
