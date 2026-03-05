1020d3b87d764b8deaab2d15f75eff3bd3278863
joho2022 <josama1997@naver.com>
2026-03-05 21:28:23 +0900

    chore: add splash route and update nickname-setup path

--- lib/ui/core/routes/route_paths.dart
+++ lib/ui/core/routes/route_paths.dart
@@ -1,6 +1,8 @@
 class RoutePaths {
+  static const String splash = '/splash';
+
   static const String login = '/login';
-  static const String nicknameSetup = '/nickname-setup';
+  static const String nicknameSetup = 'nickname-setup';
 
   static const String home = '/';
   static const String history = '/history';
